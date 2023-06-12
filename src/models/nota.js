ce831e07ac139c05de76fd8935a2d5aacf7fc259
Ronaldo <ronaldo.neto8801@gmail.com>
2023-06-12 17:48:21 -0300

    Concluir implementação da função mediaCA() e adicionar testes unitários

--- src/models/nota.js
+++ src/models/nota.js
@@ -15,8 +15,26 @@ class Nota {
     }
 
     mediaCA() {
-        return "SS";
-    }
+            const mediafinal = mediaFinal();
+      
+            
+        
+            if (media >= 9.0 && media <= 10) {
+              return "SS - Superior";
+            } else if (media >= 7.0 && media < 9.0) {
+              return "MS - Médio Superior";
+            } else if (media >= 5.0 && media < 7.0) {
+              return "MM - Médio";
+            } else if (media >= 3.0 && media < 5.0) {
+              return "MI - Médio Inferior";
+            } else if (media >= 0.1 && media < 3.0) {
+              return "II - Inferior";
+            } else {
+              return "SR - Sem rendimento";
+            }
+          
+        }
+    
 }
 
 module.exports = Nota;
